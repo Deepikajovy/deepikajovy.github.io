17142ecdeb1096705ab728052185968d1f1a9572
Deepika Jovy <deepajovy@gmail.com>
2015-03-20 23:50:07 +1300

    removed phase_0 curiculum from my site

--- unit_2/cs/week_6/exercises/23-guessing-game/GuessingGame/example_solution.cs
+++
@@ -1,9 +0,0 @@
-﻿/********************************* This File is not compiled or tested *******************************/
-
-// Mahaha there is no provided solution!
-
-//haha, you have to solve it yourself! XD
-
-// evil laughter
-
-// no seriously though, practice working off of memory. Get as far as you can :)
--- unit_2/cs/week_6/exercises/24-boggle-board/BoggleBoardChallenge/example_solution.cs
+++
@@ -1,3 +0,0 @@
-﻿/********************************* This File is not compiled or tested *******************************/
-
-// Mahaha there is no provided solution! Reach out to a fellow boot if you get stuck.
