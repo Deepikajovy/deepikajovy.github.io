31a59777bca5dc0052fa41e0c8e729a774081c41
Deepika Jovy <deepajovy@gmail.com>
2015-08-31 01:59:04 +1200

    Cleaning up the repository
    Cleaning up the repository...

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function()
-{
- 	$("#menu").accordion({collapsible: true, active:false});
-
- 	 $('img').mouseenter(function(){
- 	 	
-        $(this).effect('bounce',500);
-    });
-});
